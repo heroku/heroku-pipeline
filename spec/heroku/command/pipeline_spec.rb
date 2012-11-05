ce05fa28eee58ce39c73bf542780ab745b456530
Ryan Brainard <brainard@heroku.com>
2012-11-05 13:58:56 -0800

    place setup in contexts

--- spec/heroku/command/pipeline_spec.rb
+++ spec/heroku/command/pipeline_spec.rb
@@ -9,55 +9,59 @@ describe Heroku::Command::Pipeline do
     stub_request(:post, /api.heroku.com\/login.*/)
   end
 
-  describe "#index" do
-    before do
-      stub_pipeline_resource(:get, "dev", "downstreams").to_return(:body => JSON.generate(["staging", "prod"]))
-      heroku "pipeline -a dev"
+  context "without downstreams configured" do
+    describe "#add" do
+      before do
+        stub_pipeline_resource(:post, "staging", "downstreams", "prod")
+        heroku "pipeline:add prod -a staging"
+      end
+
+      it { should include "Added downstream app: prod" }
     end
 
-    it { should include "dev ---> staging ---> prod" }
-  end
+    describe "#remove" do
+      before do
+        stub_pipeline_resource(:delete, "staging", "downstreams", "prod")
+        heroku "pipeline:remove prod -a staging"
+      end
 
-  describe "#add" do
-    before do
-      stub_pipeline_resource(:get,  "staging", "downstreams").to_return(:body => JSON.generate([]))
-      stub_pipeline_resource(:post, "staging", "downstreams", "prod")
-      heroku "pipeline:add prod -a staging"
+      it { should include "Removed downstream app: prod" }
     end
-
-    it { should include "Added downstream app: prod" }
   end
 
-  describe "#remove" do
+  context "with downstreams configured" do
     before do
-      stub_pipeline_resource(:get,  "staging", "downstreams").to_return(:body => JSON.generate([]))
-      stub_pipeline_resource(:delete, "staging", "downstreams", "prod")
-      heroku "pipeline:remove prod -a staging"
-    end
-
-    it { should include "Removed downstream app: prod" }
-  end
-
-  describe "#diff" do
-    before do
+      stub_pipeline_resource(:get, "dev",     "downstreams").to_return(:body => JSON.generate(["staging", "prod"]))
       stub_pipeline_resource(:get, "staging", "downstreams").to_return(:body => JSON.generate(["prod"]))
-      stub_pipeline_resource(:get, "staging", "diff").to_return(:body => JSON.generate(["COMMIT_A", "COMMIT_B"]))
-      heroku "pipeline:diff -a staging"
     end
 
-    it { should include "Comparing staging to prod...done, staging ahead by 2 commits" }
-    it { should include "COMMIT_A" }
-    it { should include "COMMIT_B" }
-  end
+    describe "#index" do
+      before do
+        heroku "pipeline -a dev"
+      end
 
-  describe "#promote" do
-    before do
-      stub_pipeline_resource(:get,  "staging", "downstreams").to_return(:body => JSON.generate(["prod"]))
-      stub_pipeline_resource(:post, "staging", "promote").to_return(:body => JSON.generate("release" => "v0"))
-      heroku "pipeline:promote prod -a staging"
+      it { should include "dev ---> staging ---> prod" }
     end
 
-    it { should include "Promoting staging to prod...done, v0" }
+    describe "#diff" do
+      before do
+        stub_pipeline_resource(:get, "staging", "diff").to_return(:body => JSON.generate(["COMMIT_A", "COMMIT_B"]))
+        heroku "pipeline:diff -a staging"
+      end
+
+      it { should include "Comparing staging to prod...done, staging ahead by 2 commits" }
+      it { should include "COMMIT_A" }
+      it { should include "COMMIT_B" }
+    end
+
+    describe "#promote" do
+      before do
+        stub_pipeline_resource(:post, "staging", "promote").to_return(:body => JSON.generate("release" => "v0"))
+        heroku "pipeline:promote prod -a staging"
+      end
+
+      it { should include "Promoting staging to prod...done, v0" }
+    end
   end
 
   def stub_pipeline_resource(method, app, *extras)
